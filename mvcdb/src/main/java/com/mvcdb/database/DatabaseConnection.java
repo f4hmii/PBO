7f6e5e198e7be482571c2428e83b6b9888478d57
f4hmii <fahmif3211@gmail.com>
2025-05-28 13:29:22 +0700

    p

--- mvcdb/src/main/java/com/mvcdb/database/DatabaseConnection.java
+++ mvcdb/src/main/java/com/mvcdb/database/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/javafxdb";
+    private static final String URL = "jdbc:mysql://localhost:3306/praktikumpbo";
     private static final String USER = "root";
     private static final String PASSWORD = ""; // Ganti sesuai password MySQL kamu
 
